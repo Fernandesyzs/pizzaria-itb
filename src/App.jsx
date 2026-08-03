a5abdc0759c01a096c3ee77ccdcc4bf13c957fce
Fernandesyzs <menordasprata011@icloud.com>
2026-08-03 18:30:12 -0300

    Menu e Home do funcionário em construção

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import HomeFuncionario from './pages/HomeFuncionario/HomeFuncionario'
+
+function App() {
+
+
+  return(
+    <>
+        <div>
+
+              <HomeFuncionario/>
+        </div>
+    
+    </>
+  )
+}
+
+export default App
